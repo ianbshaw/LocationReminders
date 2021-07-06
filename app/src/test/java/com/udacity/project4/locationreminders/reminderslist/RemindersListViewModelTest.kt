2f95aeec37858eafb6045aceae0cdcc85822bf0f
Ian Bradshaw <ibradshaw@cs.unm.edu>
2021-07-06 16:16:19 -0600

    RemindersListViewModelTest.kt completed?
    SaveReminderViewModelTest.kt onclear created

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -4,7 +4,12 @@ import MainCoroutineRule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.core.IsEqual
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +20,12 @@ import org.junit.runner.RunWith
 class RemindersListViewModelTest {
 
     private lateinit var remindersListViewModel: RemindersListViewModel
+    private lateinit var datasource: FakeDataSource
+
+    private val reminder1 = ReminderDTO("Title1", "Description1", "Test1", 50.0, 50.0, "1")
+    private val reminder2 = ReminderDTO("Title2", "Description2", "Test2", 50.0, 50.0, "2")
+    private val reminder3 = ReminderDTO("Title3", "Description3", "Test3", 50.0, 50.0, "3")
+    private val reminders = listOf(reminder1, reminder2, reminder3)
 
     // Set the main coroutines dispatcher for unit testing.
     @ExperimentalCoroutinesApi
@@ -26,16 +37,26 @@ class RemindersListViewModelTest {
 
     @Before
     fun setupVM() {
-
+        datasource = FakeDataSource(reminders.toMutableList())
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(),
+            datasource)
     }
 
     @Test
     fun loadReminders_setReminderList() {
 
+        //Given
+        val reminderData1 = ReminderDataItem("Title1", "Description1", "Test1", 50.0, 50.0, "1")
+        val reminderData2 = ReminderDataItem("Title2", "Description2", "Test2", 50.0, 50.0, "2")
+        val reminderData3 = ReminderDataItem("Title3", "Description3", "Test3", 50.0, 50.0, "3")
+        val listData = listOf(reminderData1, reminderData2, reminderData3)
+
         //When loading reminders
         remindersListViewModel.loadReminders()
+        val reminderList = remindersListViewModel.remindersList.value
 
-        //Then reminder list for UI is updated
+        //Then viewmodel list equal to loaded list
+        assertThat(reminderList, IsEqual(listData))
 
     }
 
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,8 +1,22 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import MainCoroutineRule
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.core.IsEqual
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -10,7 +24,58 @@ import org.junit.runner.RunWith
 class SaveReminderViewModelTest {
 
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var datasource: FakeDataSource
 
+    private val reminderTitle = MutableLiveData<String>()
+    private val reminderDescription = MutableLiveData<String>()
+    private val reminderSelectedLocationStr = MutableLiveData<String>()
+    private val selectedPOI = MutableLiveData<PointOfInterest>()
+    private val latitude = MutableLiveData<Double>()
+    private val longitude = MutableLiveData<Double>()
+
+    private val reminders = listOf<ReminderDTO>()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupVM() {
+        reminderTitle.value = "TITLE1"
+        reminderDescription.value = "DESC1"
+        reminderSelectedLocationStr.value = "TEST"
+        latitude.value = 35.26765276556653
+        longitude.value = -106.64450596604965
+        val home = LatLng(latitude.value!!, longitude.value!!)
+        selectedPOI.value = PointOfInterest(home, "Test1", "Test2")
+
+        datasource = FakeDataSource(reminders.toMutableList())
+        saveReminderViewModel = SaveReminderViewModel(
+            ApplicationProvider.getApplicationContext(),
+            datasource)
+    }
+
+    @Test
+    fun onClear_removeAttrs() {
+        reminderTitle.value = null
+        reminderDescription.value = null
+        reminderSelectedLocationStr.value = null
+        latitude.value = null
+        longitude.value = null
+        selectedPOI.value = null
+
+        Assert.assertThat(reminderTitle.value, IsEqual(saveReminderViewModel.reminderTitle.value))
+        Assert.assertThat(reminderDescription.value, IsEqual(saveReminderViewModel.reminderDescription.value))
+        Assert.assertThat(reminderSelectedLocationStr.value, IsEqual(saveReminderViewModel.reminderSelectedLocationStr.value))
+        Assert.assertThat(latitude.value, IsEqual(saveReminderViewModel.latitude.value))
+        Assert.assertThat(longitude.value, IsEqual(saveReminderViewModel.longitude.value))
+        Assert.assertThat(selectedPOI.value, IsEqual(saveReminderViewModel.selectedPOI.value))
+
+    }
 
 }
