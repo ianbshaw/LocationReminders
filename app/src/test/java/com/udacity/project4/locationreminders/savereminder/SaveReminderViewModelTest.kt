90409016575c8efdbf1e92bcfd53f5ea235847a7
Ian Bradshaw <ibradshaw@cs.unm.edu>
2021-07-06 18:06:03 -0600

    Fixed onclear test for SaveReminderViewModelTest.kt
    Added savereminder test

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -9,11 +9,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PointOfInterest
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
-
+import com.udacity.project4.locationreminders.getOrAwaitValue
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.core.IsEqual
-import org.junit.Assert
+import org.hamcrest.core.IsNull.nullValue
+import org.hamcrest.text.IsEmptyString.isEmptyOrNullString
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,16 +26,11 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var saveReminderViewModel: SaveReminderViewModel
     private lateinit var datasource: FakeDataSource
-
-    private val reminderTitle = MutableLiveData<String>()
-    private val reminderDescription = MutableLiveData<String>()
-    private val reminderSelectedLocationStr = MutableLiveData<String>()
-    private val selectedPOI = MutableLiveData<PointOfInterest>()
-    private val latitude = MutableLiveData<Double>()
-    private val longitude = MutableLiveData<Double>()
 
     private val reminders = listOf<ReminderDTO>()
 
@@ -46,14 +44,6 @@ class SaveReminderViewModelTest {
 
     @Before
     fun setupVM() {
-        reminderTitle.value = "TITLE1"
-        reminderDescription.value = "DESC1"
-        reminderSelectedLocationStr.value = "TEST"
-        latitude.value = 35.26765276556653
-        longitude.value = -106.64450596604965
-        val home = LatLng(latitude.value!!, longitude.value!!)
-        selectedPOI.value = PointOfInterest(home, "Test1", "Test2")
-
         datasource = FakeDataSource(reminders.toMutableList())
         saveReminderViewModel = SaveReminderViewModel(
             ApplicationProvider.getApplicationContext(),
@@ -62,20 +52,44 @@ class SaveReminderViewModelTest {
 
     @Test
     fun onClear_removeAttrs() {
-        reminderTitle.value = null
-        reminderDescription.value = null
-        reminderSelectedLocationStr.value = null
-        latitude.value = null
-        longitude.value = null
-        selectedPOI.value = null
+        saveReminderViewModel.apply {
+            reminderTitle.value = "TITLE1"
+            reminderDescription.value = "DESC1"
+            reminderSelectedLocationStr.value = "LOC1"
+            latitude.value = 35.26765276556653
+            longitude.value = -106.64450596604965
+            selectedPOI.value = PointOfInterest(LatLng(latitude.value!!, longitude.value!!), "ID", "NAME")
+        }
+        saveReminderViewModel.onClear()
 
-        Assert.assertThat(reminderTitle.value, IsEqual(saveReminderViewModel.reminderTitle.value))
-        Assert.assertThat(reminderDescription.value, IsEqual(saveReminderViewModel.reminderDescription.value))
-        Assert.assertThat(reminderSelectedLocationStr.value, IsEqual(saveReminderViewModel.reminderSelectedLocationStr.value))
-        Assert.assertThat(latitude.value, IsEqual(saveReminderViewModel.latitude.value))
-        Assert.assertThat(longitude.value, IsEqual(saveReminderViewModel.longitude.value))
-        Assert.assertThat(selectedPOI.value, IsEqual(saveReminderViewModel.selectedPOI.value))
+        assertThat(saveReminderViewModel.reminderTitle.value, isEmptyOrNullString())
+        assertThat(saveReminderViewModel.reminderDescription.value, isEmptyOrNullString())
+        assertThat(saveReminderViewModel.reminderSelectedLocationStr.value, isEmptyOrNullString())
+        assertThat(saveReminderViewModel.latitude.value, `is`(nullValue()))
+        assertThat(saveReminderViewModel.longitude.value, `is`(nullValue()))
+        assertThat(saveReminderViewModel.selectedPOI.value, `is`(nullValue()))
+    }
 
+    @Test
+    fun saveReminder_addReminderToList() {
+        mainCoroutineRule.pauseDispatcher()
+
+        //When saving a reminder
+        val reminder = ReminderDataItem(title, description, location, latitude, longitude)
+        saveReminderViewModel.saveReminder(reminder)
+
+        assertThat(saveReminderViewModel.showLoading.getOrAwaitValue(), `is`(true))
+
+        mainCoroutineRule.resumeDispatcher()
+    }
+
+    companion object {
+        const val title       = "TITLE"
+        const val description = "DESCRIPTION"
+        const val location    = "LOCATION"
+        const val latitude    = 1.111
+        const val longitude   = 2.222
+        const val id          = "UUID"
     }
 
 }
